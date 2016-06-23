2e68c1310d004f1bd3ccfb63067cf9e3d356df14
Harris Miller <harrismiller08@gmail.com>
2016-06-23 17:15:58 -0600

    Adds destory session action added

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -33,4 +33,14 @@ RSpec.describe Api::V1::SessionsController, type: :controller do
     end
   end
 
+  describe "DELETE #destory" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      sign_in @user
+      delete :destroy, id: @user.auth_token
+    end
+
+    it { should respond_with 204 }
+  end
+
 end
