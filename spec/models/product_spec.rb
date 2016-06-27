628fb18e71a90c8e5892c82b38101f1d7bbd7e79
Harris Miller <harrismiller08@gmail.com>
2016-06-27 14:57:20 -0600

    Added filter scopes to the product modle

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -17,4 +17,49 @@ RSpec.describe Product, type: :model do
   it { should validate_presence_of :user_id }
 
   it { should belong_to :user }
+
+  describe "filters" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, title: "A plasma TV", price: 100
+      @product2 = FactoryGirl.create :product, title: "Fastest laptop", price: 50
+      @product3 = FactoryGirl.create :product, title: "CD player", price: 150
+      @product4 = FactoryGirl.create :product, title: "LCD TV", price: 99
+    end
+
+    describe ".filter_by_title" do
+      context "when a 'TV' title pattern is sent" do
+        it "returns the 2 products matching" do
+          expect(Product.filter_by_title("TV").size).to eql(2)
+        end
+
+        it "returns the products matching" do
+          expect(Product.filter_by_title("TV")).to match_array([@product1, @product4])
+        end
+      end
+    end
+
+    describe ".above_or_equal_to_price" do
+      it "retursn the products which are above or equal to the price" do
+        expect(Product.above_or_equal_to_price(100).sort).to match_array([@product1, @product3])
+      end
+    end
+
+    describe ".below_or_equal_to_price" do
+      it "retursn the products which are below or equal to the price" do
+        expect(Product.below_or_equal_to_price(100).sort).to match_array([@product1, @product2, @product4])
+      end
+    end
+
+    describe ".recent" do
+      before(:each) do
+        @product2.touch
+        @product3.touch
+      end
+
+      it "returns the most updated records" do
+        expect(Product.recent).to match_array([@product3, @product2, @product4, @product1])
+      end
+    end
+  end
+
 end
